0040cd1e413a60032b1fd25c3796cea917ac2b0e
Yacov Drori <yacov@drori.org>
2022-05-11 16:21:49 +0300

    clean

--- start.py
+++ start.py
@@ -1,9 +1,7 @@
 #
-# Acquires data from micro:bit using serial IO
-# Author: Lisa Ong, NUS/ISS
+# Author: Yacov Drori
 #
 
-# import helper classes for acquiring data from the Micro:bit
 
 import PySimpleGUI as sg
 import simpleGui
@@ -26,13 +24,9 @@ async def reader(mysg):
         mysg.writeSpeed(str(line, 'utf-8'))
         print(str(line, 'utf-8'))
 
-# async def main(mysg):
-    # await reader()
-    # writer = await writer()
 
 loop = get_event_loop()
 loop.create_task(writer(mysg))
 loop.create_task(reader(mysg))
 loop.run_forever()
 
-# serial4.run_loop_forever(comport, baudrate, SerialIoBase_Factory)
